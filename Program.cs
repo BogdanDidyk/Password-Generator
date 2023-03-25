9508c92905cc48607be82133b57e00b32faf6b73
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-03-25 23:35:25 +0200

    Created the PasswordGenerator class with it's fields

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        [Flags]
+        public enum PasswordChars
+        {
+            Digits = 0b0001,
+            Alphabet = 0b0010,
+            Symbols = 0b0100
+        }
+
+        const int MIN_PASSWORD_LENGTH = 5;
+        const int MAX_PASSWORD_LENGTH = 15;
+
+        readonly Random rnd;
+        readonly string DIGITS;
+        readonly string ALPHABET;
+        readonly string SPECIAL_SYMBOLS;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
